77c1fbc0ef938950032f9921a53bf00837699409
Paul Camarata <paul.camarata.ctr@navy.mil>
2018-09-04 16:30:16 -0700

    Update script much more robust

--- ShellScripts/terraformUpdater.sh
+++ ShellScripts/terraformUpdater.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+if [ -f /usr/bin/terraform ]; then
+    localVersion=$(terraform --version | cut -d "v" -f 2)
+else
+    localVersion="Not_Installed"
+fi
 
-version=$(curl https://releases.hashicorp.com/terraform/ | grep '<a href="/terraform' | head -1 | cut -d '/' -f 3)
+remoteVersion=$(curl https://releases.hashicorp.com/terraform/ | grep '<a href="/terraform' | head -1 | cut -d '/' -f 3)
 
-echo $version
+echo "local  = $localVersion"
+echo "remote = $remoteVersion"
+fileName='terraform_'$remoteVersion'_linux_amd64.zip'
 
-curl -O 'https://releases.hashicorp.com/terraform/'"$version"'/terraform_'"$version"'_linux_amd64.zip'
+echo $filePath
+
+if [ $localVersion == $remoteVersion ]; then
+    echo "Current Version " $localVersion "detected, No Change"
+else
+    echo "Updating to " $remoteVersion
+    cd ~/Downloads && curl -O 'https://releases.hashicorp.com/terraform/'"$remoteVersion"'/'"$fileName"
+    sudo unzip ~/Downloads/$fileName -d /usr/bin/
+fi
+
+if [ -f /usr/bin/terraform ]; then
+    terraform --version
+else
+    echo "Installation Failed"
+fi
