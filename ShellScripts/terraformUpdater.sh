60bde3723f56760c04ee0d8f8f6368eb9d30ef8f
Paul Camarata <paul.camarata.ctr@navy.mil>
2018-09-04 16:56:56 -0700

    added additional logic and output cleanup

--- ShellScripts/terraformUpdater.sh
+++ ShellScripts/terraformUpdater.sh
@@ -5,24 +5,30 @@ else
     localVersion="Not_Installed"
 fi
 
-remoteVersion=$(curl https://releases.hashicorp.com/terraform/ | grep '<a href="/terraform' | head -1 | cut -d '/' -f 3)
+remoteVersion=$(curl -s https://releases.hashicorp.com/terraform/ | grep '<a href="/terraform' | head -1 | cut -d '/' -f 3)
 
-echo "local  = $localVersion"
-echo "remote = $remoteVersion"
+#echo -e '\t'"local  = $localVersion" #debug 
+#echo -e '\t'"remote = $remoteVersion" #debug
+
 fileName='terraform_'$remoteVersion'_linux_amd64.zip'
 
-echo $filePath
-
-if [ $localVersion == $remoteVersion ]; then
-    echo "Current Version " $localVersion "detected, No Change"
+if [[ $localVersion == $remoteVersion ]]; then
+    echo "Current Version v" $localVersion "detected, No Change"
 else
     echo "Updating to " $remoteVersion
-    cd ~/Downloads && curl -O 'https://releases.hashicorp.com/terraform/'"$remoteVersion"'/'"$fileName"
-    sudo unzip ~/Downloads/$fileName -d /usr/bin/
+    cd ~/Downloads && curl -O 'https://releases.hashicorp.com/terraform/'"$remoteVersion"'/'"$fileName" > /dev/null 2> /dev/null
+    sudo unzip ~/Downloads/$fileName -d /usr/bin/ > /dev/null 2> /dev/null
+    
+    if [ -d ~/Documents/terraform-templates ]; then
+        cd ~/Documents/terraform-templates
+    else
+        mkdir ~/Documents/terraform-templates && cd ~/Documents/terraform-templates
+    fi
+
+    touch template.tf
+    terraform apply
 fi
 
-if [ -f /usr/bin/terraform ]; then
-    terraform --version
-else
-    echo "Installation Failed"
+if [ ! -f /usr/bin/terraform ]; then
+        echo "Installation Failed"
 fi
